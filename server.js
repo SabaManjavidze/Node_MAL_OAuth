986d5f8667024046c796a514dd0e86c3c24bf02e
SabaManjavidze <sabamanjavidze@gmail.com>
2022-02-28 22:52:14 +0400

    fixed not using port variable

--- server.js
+++ server.js
@@ -73,6 +73,6 @@ app.get("/oauth/callback", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
