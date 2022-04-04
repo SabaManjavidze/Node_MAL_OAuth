ee089ef19db5226497b33e10699b7983a7d154d1
SabaManjavidze <sabamanjavidze@gmail.com>
2022-04-04 22:55:33 +0400

    cleaned up orm config and gitignore

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,6 @@ export const config: PostgresConnectionOptions = {
   type: "postgres",
   host: "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com",
   port: 5432,
-  url: "postgres://cvcmkgqcpolvsy:9e8252a041756284737f036c04be82cc253ede4e6e29c83fa7ddb1f7bf4e8372@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/d3ubmbusnatmjf",
   username: "cvcmkgqcpolvsy",
   password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "",
